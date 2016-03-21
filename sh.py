01e160034a5b5c2ec7fc3118b0c379e76a4c2186
Jason <onejason@126.com>
2016-03-21 23:00:05 +0800

    sh

--- sh.py
+++ sh.py
@@ -9,14 +9,10 @@ url = "http://www.sse.com.cn/market/stockdata/overview/day/"
 
 wb_data=requests.get(url)
 wb_data.encoding='utf8'
-soup=BeautifulSoup(wb_data.text,'lxml')
-
-# 总市值行： 	var marketValue = '249234.14';
 text = wb_data.text
-resultList = re.findall('marketValue =',text)
-print resultList
-# beautifulSoap 使用 unicode 编码
-#anchor_tag =  soup.find('td', text=re.compile(u'市价总值'))  # 所以中文前要加u,表示unicode编码 # find('td')找到标签 #find(text=) 找到text only
-#print anchor_tag
-#volume = anchor_tag.next_sibling.text.replace(',','')
-#print volume
+# 总市值行格式： 	var marketValue = '249234.14';
+searchObj = re.search(r'marketValue = \'(\d+\.\d+)\';', text)
+if searchObj:
+   print "SH total volume: ", searchObj.group(1)
+
+# 另外的方法是先分行: text.split,再正则匹配,可能效率会高一点
